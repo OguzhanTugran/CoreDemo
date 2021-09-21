4ccfe39d42807b2fea943f3622b3540db7f94540
Oguzhan Tugran <88244702+OguzhanTugran@users.noreply.github.com>
2021-09-21 11:00:54 +0300

    CoreDemoProje

---
+++ CoreDemo/DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,9 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDal:IGenericDal<Blog>
+    {
+    }
+}
---
+++ CoreDemo/DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog Blog)
+        {
+            using var c = new Context();
+            c.Add(Blog);
+            c.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog Blog)
+        {
+            using var c = new Context();
+            c.Remove(Blog);
+            c.SaveChanges();
+        }
+        public List<Blog> ListAllBlog()
+        {
+            using var c = new Context();
+            return c.Blogs.ToList();
+        }
+
+        public void UpdateBlog(Blog Blog)
+        {
+            using var c = new Context();
+            c.Update(Blog);
+            c.SaveChanges();
+        }
+
+        public Blog GetbyId(int İd)
+        {
+            using var c = new Context();
+            return c.Blogs.Find(İd);
+        }
+    }
+}
